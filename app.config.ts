017206d30ced254814f07d6a61921d9aeaf42a3a
Trevor Keith <tk@tkeith.com>
2025-05-04 14:18:10 -0700

    basic setup: trpc, prisma, prettier, eslint

---
+++ app.config.ts
@@ -0,0 +1,50 @@
+import { createApp } from "vinxi";
+import reactRefresh from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+
+export default createApp({
+  server: {
+    preset: "node-server", // change to 'netlify' or 'bun' or anyof the supported presets for nitro (nitro.unjs.io)
+    experimental: {
+      asyncContext: true,
+    },
+  },
+  routers: [
+    {
+      type: "static",
+      name: "public",
+      dir: "./public",
+    },
+    {
+      type: "http",
+      name: "trpc",
+      base: "/trpc",
+      handler: "./src/server/api/handler.ts",
+      target: "server",
+      plugins: () => [
+        tsConfigPaths({
+          projects: ["./tsconfig.json"],
+        }),
+      ],
+    },
+    {
+      type: "spa",
+      name: "client",
+      handler: "./index.html",
+      target: "browser",
+      plugins: () => [
+        tsConfigPaths({
+          projects: ["./tsconfig.json"],
+        }),
+        TanStackRouterVite({
+          target: "react",
+          autoCodeSplitting: true,
+          routesDirectory: "./src/routes",
+          generatedRouteTree: "./src/routeTree.gen.ts",
+        }),
+        reactRefresh(),
+      ],
+    },
+  ],
+});
---
+++ src/server/api/handler.ts
@@ -0,0 +1,19 @@
+import { defineEventHandler, toWebRequest } from "@tanstack/react-start/server";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { appRouter } from "./root";
+
+export default defineEventHandler((event) => {
+  const request = toWebRequest(event);
+  if (!request) {
+    return new Response("No request", { status: 400 });
+  }
+
+  return fetchRequestHandler({
+    endpoint: "/trpc",
+    req: request,
+    router: appRouter,
+    createContext() {
+      return {};
+    },
+  });
+});
