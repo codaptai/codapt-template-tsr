ed7f20af23b100d5cfa1ef5a2a267ca93420c9aa
Trevor Keith <tk@tkeith.com>
2025-07-21 16:15:37 -0700

    template cleanup

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,7 +8,7 @@ function Home() {
   return (
     <div className="p-2">
       {/* Remove this when adding actual content/functionality */}
-      <h3 className="text-blue-500">Actual app content will go here</h3>
+      <p>Actual app content will go here</p>
     </div>
   );
 }
