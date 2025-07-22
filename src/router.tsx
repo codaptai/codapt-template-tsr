b60f5cd13324551a1b300d8bd32c1a178d571fc8
Trevor Keith <tk@tkeith.com>
2025-07-21 17:47:55 -0700

    cleanup

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,5 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { routeTree } from "./generated/tanstack-router/routeTree.gen";
-import { TRPCReactProvider } from "./trpc/react";
 
 export function createRouter() {
   const router = createTanStackRouter({
